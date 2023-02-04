e96db536f23226c4a1b3433c0600846ce3553932
Marisa <msandroni@gmail.com>
2023-02-04 20:02:35 -0300

    modificacion de font y fondo

--- cv.js
+++ cv.js
@@ -4,6 +4,19 @@ document.getElementById('mail').addEventListener('click',function() {
     window.open('mailto:lindaperez@mail.com');
 })
 
+/*Abre ventana whatsapp Web*/
 
+document.getElementById('telefono2').addEventListener('click',function(){
+    window.open('http://wa.me/+5491164886534')
+})
 
+/*Muestra/Oculta redes sociales*/
 
+function redesSociales() {
+    var x = document.getElementById("redes");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
